6aa1be24f042be5ed13aeee3fe058ef364ad952c
Sebastián Reina Balcázar <sebastian.reina_bal@uao.edu.co>
2023-03-02 23:41:34 -0500

    primer push

---
+++ InstaUAO/src/Login.jsx
@@ -0,0 +1,16 @@
+import { React } from "react";
+import Logo from './assets/img_login.jpg'
+
+const Login =()=>{
+
+    
+    return(
+        <>
+            <div>                
+                <img src={Logo}/>                       
+            </div>
+        </>
+    )
+}
+
+export default Login;
